93a854c4e94e73165b586275c86e33170dc28e41
tamsay <temitope.longe@decagon.dev>
2021-05-11 00:53:20 +0100

    url adjustment

--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ import express, {Response, Request} from "express"
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.status(200).send(`Shapes Area Calculator Homepage. Kindly visit ${req.protocol}://${req.get('host')}/fetchRecords - to see all operations performed`)
+    res.status(200).send(`Shapes Area Calculator Homepage. Kindly visit ${req.protocol}s://${req.get('host')}/fetchRecords - to see all operations performed`)
 })
 
 export default router;
